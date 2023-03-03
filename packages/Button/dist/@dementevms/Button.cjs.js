bebb39e2d679a71828f4d2a7da4428c89081e7a2
Dementevms <dementevms@gmail.com>
2023-03-03 13:25:14 +0500

    Added Button role build

--- packages/Button/dist/@dementevms/Button.cjs.js
+++ packages/Button/dist/@dementevms/Button.cjs.js
@@ -4,7 +4,9 @@ var React = require('react');
 
 var Button = function Button(_ref) {
   var children = _ref.children;
-  return /*#__PURE__*/React.createElement("button", null, children);
+  return /*#__PURE__*/React.createElement("button", {
+    role: "button"
+  }, children);
 };
 
 exports.Button = Button;
--- packages/Button/dist/@dementevms/Button.esm.js
+++ packages/Button/dist/@dementevms/Button.esm.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 var Button = function Button(_ref) {
   var children = _ref.children;
-  return /*#__PURE__*/React.createElement("button", null, children);
+  return /*#__PURE__*/React.createElement("button", {
+    role: "button"
+  }, children);
 };
 
 export { Button };
